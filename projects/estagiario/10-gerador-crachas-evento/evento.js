c55a2d3441c993ee081cbbce83ed79a9071dd7ae
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2026-09-23 10:02:01 -0300

    feat: gera os números dos crachás dos convidados e telão de entrada com uma mensagem repetida

---
+++ projects/estagiario/10-gerador-crachas-evento/evento.js
@@ -0,0 +1,25 @@
+function numerosDosCrachas(quantidade) {
+    let arr = []
+
+    for (let i = 1; i < quantidade; i++) {
+        arr.push(i)
+    }
+    
+    return arr
+}
+
+function repetirMensagem(mensagem, vezes) {
+    let arr = []
+
+    if (vezes > 0) {
+        for (let i = 1; i <= vezes; i++) {
+            arr.push(mensagem)
+        }
+
+        return arr
+    } else {
+        return arr
+    }
+}
+
+module.exports = { numerosDosCrachas, repetirMensagem }
