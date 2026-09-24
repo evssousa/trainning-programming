ede7ec96c1c141d9be3ab05a4bc982869c0257a0
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2026-09-24 17:03:04 -0300

    feat(projeto12): monitora eventos do servidor de produção

--- projects/estagiario/12-monitor-eventos-sistema/contador.js
+++ projects/estagiario/12-monitor-eventos-sistema/contador.js
@@ -13,11 +13,25 @@ function contarAteLimite(limite) {
 }
 
 function fatorial(n) {
+    let resultado = 1
+    let contador = 1
 
+    while (contador <= n) {
+        resultado = resultado * contador
+        contador++
+    }
+    return resultado
 }
 
 function processarFila(fila) {
+    const arr = []
+    let contador = 0
 
+    while (contador < fila.length) {
+        arr.push(`processado: ${fila[contador]}`)
+        contador++
+    }
+    return arr
 }
 
 module.exports = {
